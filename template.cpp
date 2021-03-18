edcc06b06d410d501e1bb4bce2ad1318e4821df4
kajal1sharma <kajal1231995@gmail.com>
2021-03-18 15:47:53 +0530

    linked list 18/3

---
+++ template.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+    
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+void print(Node *head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+bool findkey(Node* head,int key){
+    while(head!=NULL){
+        if(key==head->data){
+            return true;
+        }
+        head=head->next;
+    }
+    return false;
+}
+
+int main()
+{
+    Node *head=new Node(12);
+    head->next=new Node(14);
+    head->next->next=new Node(24);
+    head->next->next->next=new Node(224);
+    head->next->next->next->next=new Node(17);
+    head->next->next->next->next->next=new Node(56);
+    head->next->next->next->next->next->next=new Node(89);
+    head->next->next->next->next->next->next->next=new Node(23);
+    print(head);
+    
+    bool b=findkey(head,224);
+    if(b==false){
+        cout<<"not found";
+    }
+    else{
+        cout<<"found";
+    }
+    
+    return 0;
+}
